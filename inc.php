93a1b574bc7c7c18c4135e83677147f0cdba1bcd
MadCornDog <56613295+HaydenFrobenius@users.noreply.github.com>
2022-07-24 00:45:04 -0400

    Update inc.php

--- inc.php
+++ inc.php
@@ -1,3 +1,5 @@
-<?php
-session_start();
-$con = mysqli_connect("localhost","id17760279_madcorndog","12876543Ab!Ab!","id17760279_chatapp");
+<?php
+
+session_start();
+
+$con = mysqli_connect("localhost","root","","chat");
